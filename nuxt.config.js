a69314b0c5650fb5216922a4f8915f93e3e58067
Kasra Karami <kasra.karami.kk@gmail.com>
2021-05-28 21:42:38 +0430

    add light theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,6 +83,24 @@ export default {
       default: 'dark',
       disable: false,
       themes: {
+        light: {
+          primary: '#003974',
+          secondary: '#A7A8A9',
+          warning: '#FEC233',
+          error: '#F51F1F',
+          success: '#32BE93',
+          info: '#2196F3',
+          accent: '#82B1FF',
+          // link
+          anchor: '#003974',
+          // colors
+          blue: '#003974',
+          grey: '#A7A8A9',
+          gray: '#D0D0CE',
+          yellow: '#FEC233',
+          red: '#F51F1F',
+          green: '#32BE93',
+        },
         dark: {
           // primary: colors.blue.darken2,
           // accent: colors.grey.darken3,
